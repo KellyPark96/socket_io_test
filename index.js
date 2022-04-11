8db2928c125801bc67679308efe2842c81740e59
KellyPark96 <kellypark2032@gmail.com>
2022-04-12 00:14:24 +0900

    add socket.io

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 import express from "Express";
 
 const app = express();
-
 const PORT = 4000;
-
 const http = require("http");
-
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.get("/", (req, res) => {
-	res.send("<h1>Hello World</h1>");
+const handleHome = (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+	// return res.send("<h1>Hello World</h1>");
+};
+
+app.get("/", handleHome);
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
+	});
 });
 
 server.listen(PORT, () => {
-	console.log("listening on *:3000");
+	console.log("listening on *:4000");
 });
